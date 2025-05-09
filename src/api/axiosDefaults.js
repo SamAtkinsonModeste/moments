011c00a4dee74582972647041e2329eef8c2cb74
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-09 13:50:02 +0100

    feat: implement SignUpForm component and update routing in App.js
    -   style: add styles for SignUpForm and Button components
    chore: create axiosDefaults for API configuration

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://bn-api-code-institute-6164ccee29f7.herokuapp.com/"
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data"
+axios.defaults.withCredentials = true
