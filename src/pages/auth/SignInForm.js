53b9315fe5b1a482f892d31d62ca15e37dd2a9f6
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-10 20:13:58 +0100

    feat: Users can now log in and are directed to home page once looged in.      -   implement sign-in functionality with form handling and API integration

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,16 +14,39 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
 const SignInForm = () => {
   //   Add your component logic here
+  const [signInData, setSignInData] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+  const history = useHistory();
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {}
+  };
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>SIGN IN</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -31,6 +54,8 @@ const SignInForm = () => {
                 type="text"
                 placeholder="Enter Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -41,6 +66,8 @@ const SignInForm = () => {
                 type="password"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
 
