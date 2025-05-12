70f59a596ad3a0e8268deb6d6b87f487183221c2
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-12 12:00:43 +0100

    fix: update axios base URL and add console log for login response in SignInForm

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://bn-api-code-institute-6164ccee29f7.herokuapp.com/";
+axios.defaults.baseURL = "https://be-api-7ff643bbf9fb.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
