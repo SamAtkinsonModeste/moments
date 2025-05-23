d00e0c83ac28fd1d89528750c6a19959d0586706
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-23 15:12:37 +0100

    feat: display users profile info

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,7 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { axiosReq } from "../api/axiosDefaults";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
-import { followHelper, unfollowHelper } from "../utils/utils";
 
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
@@ -17,54 +16,6 @@ export const ProfileDataProvider = ({ children }) => {
   });
 
   const currentUser = useCurrentUser();
-
-  const handleFollow = async (clickedProfile) => {
-    try {
-      const { data } = await axiosRes.post("/followers/", {
-        followed: clickedProfile.id,
-      });
-
-      setProfileData((prevState) => ({
-        ...prevState,
-        pageProfile: {
-          results: prevState.pageProfile.results.map((profile) =>
-            followHelper(profile, clickedProfile, data.id)
-          ),
-        },
-        popularProfiles: {
-          ...prevState.popularProfiles,
-          results: prevState.popularProfiles.results.map((profile) =>
-            followHelper(profile, clickedProfile, data.id)
-          ),
-        },
-      }));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleUnfollow = async (clickedProfile) => {
-    try {
-      await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
-
-      setProfileData((prevState) => ({
-        ...prevState,
-        pageProfile: {
-          results: prevState.pageProfile.results.map((profile) =>
-            unfollowHelper(profile, clickedProfile)
-          ),
-        },
-        popularProfiles: {
-          ...prevState.popularProfiles,
-          results: prevState.popularProfiles.results.map((profile) =>
-            unfollowHelper(profile, clickedProfile)
-          ),
-        },
-      }));
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   useEffect(() => {
     const handleMount = async () => {
@@ -86,9 +37,7 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider
-        value={{ setProfileData, handleFollow, handleUnfollow }}
-      >
+      <SetProfileDataContext.Provider value={setProfileData}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
