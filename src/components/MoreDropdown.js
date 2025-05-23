620dfbe616cf70a306b8986862e4fe581e89470c
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-23 22:27:14 +0100

    feat: user can edit their profile page

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/MoreDropdown.module.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useHistory } from "react-router-dom";
 
 //  NOTE The forwardRef is important!!
 //  NOTE Dropdown needs access to the DOM node in order to position the Menu
@@ -43,6 +44,37 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
           aria-label="delete"
         >
           <i className="fas fa-trash-alt" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export const ProfileEditDropdown = ({ id }) => {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-edit" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
